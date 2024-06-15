ee2cd44e03a29d14c8873b28563c10a3d0e383fe
Sergii Povarchuk <sergii.povarchuk@computools.com>
2024-06-15 11:41:45 +0300

    Add project files.

---
+++ ServerApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using ServerApp;
+using System.Xml;
+using System.Xml.Linq;
+
+Console.WriteLine("Generate infinity numbers");
+
+
+// This is a technical task for an interview.
+// Range and multicast group are configured via a separate XML config
+
+try
+{
+    string? path = Environment.CurrentDirectory;
+
+    // Find the file in the local directories
+    DirectoryInfo directory = new DirectoryInfo(path);
+    DirectoryInfo parentDirectory = directory.Parent;
+    DirectoryInfo projectDirectory = parentDirectory?.Parent;
+    DirectoryInfo xmlLocation = projectDirectory?.Parent;
+
+
+    if (xmlLocation == null)
+    {
+        Console.WriteLine("Can't to find the XML location.");
+        return;
+    }
+
+    string configPath = Path.Combine(xmlLocation.FullName, "config.xml");
+    XDocument document = XDocument.Load(configPath);
+
+    string? multicastAddress = document.Root.Element("multicastAddress")?.Value;
+    if (!int.TryParse(document.Root.Element("range")?.Value, out int range))
+    {
+        Console.WriteLine("Invalid range value in config.xml");
+        return;
+    }
+    if (!int.TryParse(document.Root.Element("port")?.Value, out int port))
+    {
+        Console.WriteLine("Invalid port value in config.xml");
+        return;
+    }
+
+
+    Server.Sendler(multicastAddress, range, port);
+
+
+
+}
+catch (IOException ex)
+{
+    Console.WriteLine($"IO Exception: {ex}");
+}
+catch (XmlException ex)
+{
+    Console.WriteLine($"XML Exception: {ex}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unexpected error: {ex}");
+}
+
---
+++ ServerApp/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    public static class Server
+    {
+        // This server method sends data to the client
+        public static void Sendler(string? multicastAddress, int range, int port)
+        {
+            
+            UdpClient udpClient = new UdpClient();
+            udpClient.JoinMulticastGroup(IPAddress.Parse(multicastAddress));
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(multicastAddress), port);
+
+            Random random = new Random();
+            while (true)
+            {
+                int number = random.Next(range);
+                byte[] buffer = Encoding.ASCII.GetBytes(number.ToString());
+                udpClient.Send(buffer, buffer.Length, remoteEndPoint);
+            }
+        }
+    }
+}
